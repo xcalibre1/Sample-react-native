4edddfedd9b5e9b6a3ffc7f8fb0c6ce36a59dcda
User <user@Users-MacBook-Pro.local>
2023-12-18 19:56:01 +0530

    first commit by yash

---
+++ App.tsx
@@ -0,0 +1,30 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  Text,
+  View,
+} from 'react-native';
+
+
+
+function App(){
+  
+  return (
+    <SafeAreaView >
+    
+        <View>
+          <Text style={{color: "red", fontSize: 40, marginTop: 40}}>Lets Begin </Text>
+        </View>
+    </SafeAreaView>
+  );
+}
+
+
+export default App;
